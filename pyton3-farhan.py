a5e4bac818a718cb1ef34b982239171f6a3cf70f
mfarhanamrullah <111480127+mfarhanamrullah@users.noreply.github.com>
2022-08-21 22:13:30 -0700

    Add files via upload

---
+++ pyton3-farhan.py
@@ -0,0 +1,15 @@
+n=input("masukan nama depan: ")
+b=input("masukan nama belakng: ")
+print("nama lengkap anda adalah",n,b)
+
+a=int(input("masukan nilai A: "))
+b=float(input("masukan nilai B: "))
+c=a+b
+
+print("A=", a)
+print("B=", b)
+print(a,"+",b,"+",'c')
+
+x= (input("masukan nilai mata uang; "))
+y= x *13752
+print(x,"dollar=",y,"rupiah")
