7b265cef89dd29d05a597fdb79683ea09618bec0
mfarhanamrullah <111480127+mfarhanamrullah@users.noreply.github.com>
2023-02-13 09:50:58 +0700

    Add files via upload

---
+++ latihan_fungsi.py
@@ -0,0 +1,41 @@
+''' fungsi pada def'''
+
+''' Def nama_fungsi():'''
+
+def salam():
+    print("asamualakum !! akhi  ukhty")
+
+    salam()
+    salam()
+    salam()
+
+
+
+def istigfar():
+    print("astagfirullah")
+
+istigfar()
+istigfar()
+istigfar()
+
+'''fungsi dengan parameter'''
+'''def nama_fungsi(p1, p2)'''
+
+def say_hi(nama):
+    print("halo perkenalkan nama saya", nama)
+
+say_hi("jawir")
+say_hi("gondrong")
+
+def perkenalan(nama, alamat, umur):
+    print("nama saya:", nama)
+    print("alamat saya:", alamat)
+    print("umur saya:", umur,)
+
+perkenalan("bono","haji icang",20)
+
+
+
+
+
+
